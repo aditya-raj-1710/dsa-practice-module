03a396af41e5be4d557a843f839170329f5d8293
Aditya Raj <rajaditya.1710@gmail.com>
2025-10-28 00:07:33 +0530

    Adds MIN Integer handling inMathPower

--- array-problems-2/MathPower.java
+++ array-problems-2/MathPower.java
@@ -1,13 +1,16 @@
 public class MathPower {
     public static void main(String[] args) {
         MathPowerSolution solution = new MathPowerSolution();
-        double value = solution.myPow(2,-2);
+        double value = solution.myPow(2,-2147483648);
         System.out.println(value);
     }
 }
 class MathPowerSolution {
     public double myPow(double x, int n) {
         //your code goes here
+        if(n == Integer.MIN_VALUE){
+            return (1/x)*1/power(x,-(n+1));
+        }
         if(n<0){
             return 1/power(x,-n);
         }
