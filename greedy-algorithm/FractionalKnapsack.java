556e80a8b5c2c431a56c2e0f6532ae4410049faf
Aditya Raj <rajaditya.1710@gmail.com>
2025-11-27 09:47:09 +0530

    Adds FractionalKnapsack problem - leetcode problem

--- greedy-algorithm/FractionalKnapsack.java
+++ greedy-algorithm/FractionalKnapsack.java
@@ -9,6 +9,11 @@ public class FractionalKnapsack {
         FractionalKnapsackSolution s = new FractionalKnapsackSolution();
         System.out.println(s.fractionalKnapsack(val,wt,capacity));
         System.out.printf("%.6f", s.fractionalKnapsack(val, wt, capacity));
+        System.out.println();
+
+        int[][] boxTypes = {{1,3},{2,2},{3,1}};
+        int truckSize=4;
+        System.out.println(s.maximumUnits(boxTypes,truckSize));
     }
 }
 class FractionalKnapsackSolution {
@@ -39,6 +44,23 @@ class FractionalKnapsackSolution {
         }
 
         return Math.round(totalValue * 1e6) / 1e6;
+    }
+
+    /// LeetCode knapsack problem
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        int n= boxTypes.length;
+        Arrays.sort(boxTypes, (a,b) -> b[1]-a[1]);
+
+        int i=0;
+        int result =0;
+
+        while(truckSize>0 && i<n){
+            int keep = Math.min(truckSize,boxTypes[i][0]);
+            truckSize -= keep;
+            result += keep * boxTypes[i][1];
+            i++;
+        }
+        return result;
     }
 }
 
