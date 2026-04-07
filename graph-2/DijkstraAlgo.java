32e3426319e2f2c970ce056b62558e4e0198984a
Aditya Raj <rajaditya.1710@gmail.com>
2026-04-07 08:30:15 +0530

    Adds DijkstraAlgo problem - Set implementation

--- graph-2/DijkstraAlgo.java
+++ graph-2/DijkstraAlgo.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class DijkstraAlgo {
     public static void main(String[] args) {
@@ -17,6 +14,14 @@ public class DijkstraAlgo {
         DijkstraAlgoSolution sol = new DijkstraAlgoSolution();
 
         int[] ans = sol.dijkstraPriorityQueue(V, adj, S);
+
+        System.out.print("The shortest distance of nodes from the source node is: ");
+        for (int i = 0; i < V; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println();
+
+        ans = sol.dijkstraSet(V, adj, S);
 
         System.out.print("The shortest distance of nodes from the source node is: ");
         for (int i = 0; i < V; i++) {
@@ -58,6 +63,47 @@ class DijkstraAlgoSolution{
                 if(dis + edgeWt < dist[adjNode]){
                     dist[adjNode] = dis+edgeWt;
                     pq.add(new int[]{dist[adjNode],adjNode});
+                }
+            }
+        }
+        return dist;
+    }
+
+    public int[] dijkstraSet(int V, ArrayList<ArrayList<Integer>> edges, int S) {
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+
+        for (ArrayList<Integer> edge : edges) {
+            int u = edge.get(0), v = edge.get(1), w = edge.get(2);
+            adj.get(u).add(new int[] {v, w});
+            adj.get(v).add(new int[] {u, w});
+        }
+
+        TreeSet<int[]> set =
+                new TreeSet<>(
+                        (a, b) -> {
+                            if (a[0] != b[0]) return Integer.compare(a[0], b[0]);
+                            return Integer.compare(a[1],b[1]);
+                        });
+
+        int[] dist = new int[V];
+        Arrays.fill(dist, (int) 1e9);
+        dist[S] = 0;
+        set.add(new int[] {0, S});
+
+        while (!set.isEmpty()) {
+            int[] current = set.pollFirst();
+            int dis = current[0];
+            int node = current[1];
+
+            for (int[] it : adj.get(node)) {
+                int adjNode = it[0];
+                int edgeWt = it[1];
+
+                if (dis + edgeWt < dist[adjNode]) {
+                    set.remove(new int[]{dist[adjNode], adjNode});
+                    dist[adjNode] = dis + edgeWt;
+                    set.add(new int[] {dist[adjNode], adjNode});
                 }
             }
         }
