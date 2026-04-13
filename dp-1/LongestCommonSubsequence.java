081d0c3b6a51030627bc8ab7226bba5738b35c26
Aditya Raj <rajaditya.1710@gmail.com>
2026-04-13 23:20:17 +0530

    Adds LongestCommonSubsequence problem - tabulation approaches

--- dp-1/LongestCommonSubsequence.java
+++ dp-1/LongestCommonSubsequence.java
@@ -7,13 +7,64 @@ public class LongestCommonSubsequence {
 
         LongestCommonSubsequenceSolution sol = new LongestCommonSubsequenceSolution();
 
-        System.out.println("The Length of Longest Common Subsequence is " + sol.lcs(s1, s2));
+        System.out.println("The Length of Longest Common Subsequence is " + sol.lcs1(s1, s2));
+        System.out.println("The Length of Longest Common Subsequence is " + sol.lcs2(s1, s2));
+        System.out.println("The Length of Longest Common Subsequence is " + sol.lcs3(s1, s2));
     }
 }
 
 
 class LongestCommonSubsequenceSolution {
-    public int lcs(String str1, String str2) {
+
+    ///Tabulation - space optimization
+    public int lcs3( String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int[] prev = new int[m+1];
+        int[] curr = new int[m+1];
+
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=m;j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    curr[j] = 1 + prev[j-1];
+                }else{
+                    curr[j] = Math.max(prev[j],curr[j-1]);
+                }
+            }
+            System.arraycopy(curr,0,prev,0,m+1);
+        }
+        return prev[m];
+    }
+
+    /// Tabulation
+    public int lcs2( String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=m;j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    /// Memoization
+    public int lcs1(String str1, String str2) {
         int n = str1.length();
         int m = str2.length();
         int[][] dp = new int[n][m];
