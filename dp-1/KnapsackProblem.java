216a2071906d1ece79436c5e895db8d63c76ff2a
Aditya Raj <rajaditya.1710@gmail.com>
2026-04-15 23:13:24 +0530

    Adds KnapsackProblem - tabular + space optimization approaches

--- dp-1/KnapsackProblem.java
+++ dp-1/KnapsackProblem.java
@@ -10,10 +10,56 @@ public class KnapsackProblem {
         KnapsackProblemSolution sol = new KnapsackProblemSolution();
 
         System.out.println("The Maximum value of items is " + sol.knapsack01(wt, val, n, W));
+        System.out.println("The Maximum value of items is " + sol.knapsack02(wt, val, n, W));
+        System.out.println("The Maximum value of items is " + sol.knapsack03(wt, val, n, W));
     }
 }
 
 class KnapsackProblemSolution {
+    /// Space optimization
+    public int knapsack03(int[] wt, int[] val, int n, int W) {
+        int[] prev = new int[W+1];
+
+        for(int i= wt[0]; i <= W;i++){
+            prev[i] = val[0];
+        }
+
+        for(int ind = 1; ind <n;ind++){
+            for(int cap =W;cap >= 0;cap--){
+                int notTaken = prev[cap];
+                int taken = Integer.MIN_VALUE;
+
+                if(wt[ind] <= cap){
+                    taken = val[ind] + prev[cap-wt[ind]];
+                }
+                prev[cap]=Math.max(notTaken,taken);
+            }
+        }
+        return prev[W];
+    }
+
+    ///  Tabularisation
+    public int knapsack02(int[] wt, int[] val, int n, int W) {
+        int[][] dp = new int[n][W+1];
+
+        for(int i= wt[0]; i <= W;i++){
+            dp[0][i] = val[0];
+        }
+
+        for(int ind = 1; ind <n;ind++){
+            for(int cap =0;cap <= W;cap++){
+                int notTaken = dp[ind-1][cap];
+                int taken = Integer.MIN_VALUE;
+
+                if(wt[ind] <= cap){
+                    taken = val[ind] + dp[ind-1][cap-wt[ind]];
+                }
+                dp[ind][cap]=Math.max(notTaken,taken);
+            }
+        }
+        return dp[n-1][W];
+    }
+
 
     /// Recursion + Memoization
     public int knapsack01(int[] wt, int[] val, int n, int W) {
