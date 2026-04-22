e75699a57a875144fc381bb6d001e66eca25bef5
Aditya Raj <rajaditya.1710@gmail.com>
2026-04-22 23:32:03 +0530

    Adds SubsetSum problem - tabulation + space optimization implementation

--- dp-2/SubsetSum.java
+++ dp-2/SubsetSum.java
@@ -11,10 +11,63 @@ public class SubsetSum {
             System.out.println("Subset with the given target found");
         else
             System.out.println("Subset with the given target not found");
+
+        if (sol.isSubsetSum2(arr, target))
+            System.out.println("Subset with the given target found");
+        else
+            System.out.println("Subset with the given target not found");
+
+        if (sol.isSubsetSum3(arr, target))
+            System.out.println("Subset with the given target found");
+        else
+            System.out.println("Subset with the given target not found");
     }
 }
 
 class SubsetSumSolution {
+    /// Tabulation - Space optimization
+    public boolean isSubsetSum3(int[] arr, int target) {
+        int n = arr.length;
+        boolean[] prev= new boolean[target+1];
+        prev[0] = true;
+
+        if(arr[0] <= target) prev[arr[0]] = true;
+
+        for(int ind=1;ind<n;ind++){
+            boolean[] curr = new boolean[target+1];
+            curr[0] = true;
+            for(int k=1;k<=target;k++){
+                boolean notTake = prev[k];
+                boolean take = false;
+                if(arr[ind] <= k){
+                    take = prev[k-arr[ind]];
+                }
+                curr[k] = take || notTake;
+            }
+            prev=curr;
+        }
+        return prev[target];
+    }
+
+    /// Tabulation
+    public boolean isSubsetSum2(int[] arr, int target) {
+        int n = arr.length;
+        boolean[][] dp = new boolean[n][target+1];
+        for(int i=0;i<n;i++)dp[i][0] = true;
+        if(arr[0] <= target) dp[0][arr[0]] = true;
+
+        for(int ind=1;ind<n;ind++){
+            for(int k=1;k<=target;k++){
+                boolean notTake = dp[ind-1][k];
+                boolean take = false;
+                if(arr[ind] <= k){
+                    take = dp[ind-1][k-arr[ind]];
+                }
+                dp[ind][k] = take || notTake;
+            }
+        }
+        return dp[n-1][target];
+    }
 
     /// Recursion + Memoization
     public boolean isSubsetSum(int[] arr, int target) {
